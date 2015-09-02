8ee37ba584c6d5fa5f882d2b1d69d4c5bfeca5dc
illan <illanpalao@gmail.com>
2015-09-02 17:34:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,4 @@
 });
 */
 
-function onSuccess(contacts) {
-    alert('Found ' + contacts.length + ' contacts.');
-    alert(JSON.stringify(contacts));
-};
-
-function onError(contactError) {
-    alert('onError!');
-};
-
-// find all contacts with 'Bob' in any name field
-var options      = new ContactFindOptions();
-options.filter   = "Patri";
-options.multiple = true;
-var fields       = ["displayName", "name", "phoneNumbers"];
-navigator.contacts.find(fields, onSuccess, onError, options);
-
+window.location = "http://www.google.es"
